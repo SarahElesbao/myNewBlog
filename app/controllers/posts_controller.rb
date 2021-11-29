c4f2816f61aa1a126bfc0f20ddb6db82286da9b8
Sarah Elesbao <sarahelesbao12@gmail.com>
2021-11-29 17:33:38 -0300

    remove admin_id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,7 +25,6 @@ class PostsController < ApplicationController
   # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
-    @post.admin = current_admin
 
     respond_to do |format|
       if @post.save
